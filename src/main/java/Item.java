5531435c71f8b16362075b858e92c10c8051c5ff
pnowak86 <p_nowak86@poczta.fm>
2017-06-08 19:57:43 +0200

    commit with changes item

---
+++ src/main/java/Item.java
@@ -0,0 +1,23 @@
+/**
+ * Created by RENT on 2017-06-08.
+ */
+public class Item {
+    private String name;
+    private int price;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+
+    public Item(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+
+}
